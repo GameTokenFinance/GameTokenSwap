62639871e370c79126731244548a445d22f38ff7
GameTokenFinance <info@gametoken.finance>
2021-04-11 14:16:18 +0300

    swap

---
+++ src/components/Menu/config.ts
@@ -0,0 +1,69 @@
+import { MenuEntry } from '@gametoken/uikit'
+
+const config: MenuEntry[] = [
+  {
+    label: 'Home',
+    icon: 'HomeIcon',
+    href: 'https://gametokenfi.netlify.app',
+  },
+  {
+    label: 'Trade',
+    icon: 'TradeIcon',
+    items: [
+      {
+        label: 'Exchange',
+        href: '/swap',
+      },
+      {
+        label: 'Liquidity',
+        href: '/pool',
+      },
+    ],
+  },
+  {
+    label: 'Farming',
+    icon: 'FarmIcon',
+    href: 'https://gametokenfi.netlify.app',
+  },
+  {
+    label: 'Pools',
+    icon: 'PoolIcon',
+    href: 'https://gametokenfi.netlify.app/pools',
+  },
+  {
+    label: 'GameMarket',
+    icon: 'IfoIcon',
+    href: 'https://market.gametoken.finance',
+  },
+  {
+    label: 'GameDollar',
+    icon: 'IfoIcon',
+    href: 'https://gdollar.gametoken.finance',
+  },
+  {
+    label: 'GameVault',
+    icon: 'IfoIcon',
+    href: 'https://gvault.gametoken.finance',
+  },
+
+  {
+    label: 'More',
+    icon: 'MoreIcon',
+    items: [
+      {
+        label: 'Github',
+        href: 'https://github.com/GameTokenFinance',
+      },
+      {
+        label: 'Docs',
+        href: 'https://docs.gametoken.finance/',
+      },
+      {
+        label: 'Blog',
+        href: 'https://medium.com/@GameTokenFi',
+      },
+    ],
+  },
+]
+
+export default config
