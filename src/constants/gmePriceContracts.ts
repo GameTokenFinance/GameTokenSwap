38f7b93e8a94d8142ba678501ceec067d381fe97
GameTokenFinance <info@gametoken.finance>
2021-04-18 15:02:07 +0300

    price fix

--- src/constants/gmePriceContracts.ts
+++ src/constants/gmePriceContracts.ts
@@ -1,7 +1,7 @@
 const priceContracts: {gmeAddress: string, bnbAddress: string, lpAddress:string} = {
     gmeAddress: '0xA2e62D43B5340E269Cad0A06D4813957BC56cA31',
     bnbAddress: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
-    lpAddress: '0xddBf610d1F2aaC3D532622459014696FCf0904Ea'
+    lpAddress: '0x56CFF0753F898414099CC7E9bA337914AA7215d7'
   }
   
   export default priceContracts
